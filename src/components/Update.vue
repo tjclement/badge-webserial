5633a98c6a4b71a0b82929585d533d352795e356
Tom <mr@tomclement.nl>
2021-03-07 20:32:44 +0100

    Revert "Made update view use HTTP instead of HTTPS because of legacy cert in use there"
    This reverts commit ac3b357b8d215ffd13bb3c143458ee41aeb00c77.

--- src/components/Update.vue
+++ src/components/Update.vue
@@ -63,7 +63,7 @@
                 component.badge_firmware_version = parseInt(version);
                 component.checking_badge = false;
 
-                fetch('http://ota.badge.team/version/'+repo_name+'.txt',{mode:'cors'})
+                fetch('https://ota.badge.team/version/'+repo_name+'.txt',{mode:'cors'})
                     .then(response => {response.json().then((version) => {
                         component.server_firmware_version = version.build;
                         component.server_firmware_name = version.name;
